1b2454ddb7fcfbc5aa47502f24bfa89b3b6604ca
Nicolas Kutik <122318205+Nicolas-Kutik@users.noreply.github.com>
2025-02-09 15:41:37 -0500

    Commit 6: -> Added Export on recipes.js and Recipe.js -> Reworked the app.js to suit example done in class

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const swagger = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const recipeRouting = require('./routes/recipeRoutes')
-require('dotenv').config;
+import express from 'express';
+import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import mongoose from 'mongoose';
+import recipeRouting from './routes/recipeRoutes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Controllers
+import recipeController from './controllers/recipe.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware Setup
-app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/recipes', recipeRouting);
+app.use(bodyParser.json());
+
+// Swagger config
+const docOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Recipes API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['./routes/*.js']
+};
+
+const openapiSpecification = swaggerJSDoc(docOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(`Connection Failure: ${err}`));
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// URL dispatching
+app.use('/api/v1/recipes', recipeRouting);
+
+// Start web server
+app.listen(port, () => {
+    console.log(`Express API running on port ${port}`);
+});
