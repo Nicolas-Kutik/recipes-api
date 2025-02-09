144e808e30a8a506f49f23353cd3cbd441711ee1
Nicolas Kutik <122318205+Nicolas-Kutik@users.noreply.github.com>
2025-02-09 14:40:32 -0500

    Commit 3: Created Controller Folder -> Renamed server.js to app.js -> Completed the recipe Controller -> Created vercel.json -> Created swagger.json

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -20,3 +20,36 @@ exports.getAllRecipes = async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 };
+
+// Get Single Recipe
+exports.getRecipesById = async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id, req.params, req.body);
+        if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+        res.json(recipe);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Update Recipe
+exports.updateRecipe = async (req, res) => {
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedRecipe) return res.status(404).json({ message: 'Recipe not found' });
+        res.json(updatedRecipe);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Delete Recipe
+exports.deleteRecipe = async (req, res) => {
+    try {
+        const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id, req.params);
+        if (!deletedRecipe) return res.status(404).json({ message: 'Recipe not found' });
+        res.json(deletedRecipe);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
