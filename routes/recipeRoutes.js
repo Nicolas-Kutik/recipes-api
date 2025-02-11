5242760bc354baa0cca053cfd312696cfc4e59f9
Nicolas Kutik <122318205+Nicolas-Kutik@users.noreply.github.com>
2025-02-10 21:51:07 -0500

    Commit 8: -> Updated Routing

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -3,10 +3,10 @@ import recipeController from '../controllers/recipe.js';
 
 const router = express.Router();
 
-router.post('/api/recipe', recipeController.createRecipe);
-router.get('/api/recipe/:id', recipeController.getRecipeById);
-router.get('/api/recipes', recipeController.getAllRecipes);
-router.put('/api/recipe/:id', recipeController.updateRecipe);
-router.delete('/api/recipe/:id', recipeController.deleteRecipe);
+router.post('/api/v1/recipe', recipeController.createRecipe);
+router.get('/api/v1/recipe/:id', recipeController.getRecipeById);
+router.get('/api/v1/recipes', recipeController.getAllRecipes);
+router.put('/api/v1/recipe/:id', recipeController.updateRecipe);
+router.delete('/api/v1/recipe/:id', recipeController.deleteRecipe);
 
 export default router;
